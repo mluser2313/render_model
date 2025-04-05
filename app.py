f60488fd3ef220258d201e589d4eeeac42a6341b
ahsan.t2313 <ahsandasavage@gmail.com>
2025-04-05 22:34:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -153,4 +153,4 @@ def predict():
     return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=10000, debug=False)
