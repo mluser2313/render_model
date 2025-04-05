ae9ebd7e995add310df8159111ede8bd3e90b44b
ahsan.t2313 <ahsandasavage@gmail.com>
2025-04-05 22:49:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -153,4 +153,5 @@ def predict():
     return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000, debug=False)
+    port = int(os.environ.get('PORT', 8080))  # Use the PORT environment variable or fallback to 8080
+    app.run(host='0.0.0.0', port=port, debug=False)
